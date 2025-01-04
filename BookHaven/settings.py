6f506c842193a6efc1f53e12060f49f56fdf9828
nandini2727 <nandinu2727@gmail.com>
2025-01-04 14:16:56 +0530

    Created books model and added wishlist page

--- BookHaven/settings.py
+++ BookHaven/settings.py
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'home',
     'accounts',
+    "books",
 ]
 
 MIDDLEWARE = [
@@ -129,6 +130,8 @@ STATICFILES_DIRS = [
     BASE_DIR / "static",
 ]
 
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
