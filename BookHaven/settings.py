a570fbacf7fa2343ea0a2b7bc55a7d116dc961ea
nandini2727 <nandiniu2727@gmail.com>
2025-02-17 23:51:49 +0530

    Switch to PostgreSQL

--- BookHaven/settings.py
+++ BookHaven/settings.py
@@ -27,10 +27,10 @@ ALLOWED_HOSTS=['*']
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-!@4-m3ru9@$%qvdjmyk7=65au-vh_pfn^ohz6m)w67zspm!u$e'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["*"]
 
@@ -93,8 +93,12 @@ WSGI_APPLICATION = 'BookHaven.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.getenv('DB_NAME'),
+        'USER': os.getenv('DB_USER'),
+        'PASSWORD': os.getenv('DB_PASSWORD'),
+        'HOST': os.getenv('DB_HOST'),
+        'PORT': os.getenv('DB_PORT'),
     }
 }
 
